9f5172fd00957db22e82359c9caf78a37bc1092d
Haades-dev <farizarkn3@gmail.com>
2026-02-11 09:04:52 +0700

    Delete P1 directory

--- P1/js/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', ()=>{
-	const btn = document.querySelector('.actions .btn');
-	if(btn){
-		btn.addEventListener('click', (e)=>{
-			// optional: beri feedback singkat saat mengunduh
-			setTimeout(()=>alert('Terima kasih! CV akan diunduh.'), 200);
-		});
-	}
-});
